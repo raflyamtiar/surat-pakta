55320606da657718f35a11a54f92d35039b5fe32
raflyamtiar <raflyamtiar93@gmail.com>
2024-08-13 14:43:20 +0700

    update front-end

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const menuList = document.getElementById("menu-list");
 iconNav.addEventListener("click", () => {
   menuList.classList.toggle("hidden");
 });
+
+function togglePopup(popupId) {
+  document.getElementById(popupId).classList.toggle("active");
+}
